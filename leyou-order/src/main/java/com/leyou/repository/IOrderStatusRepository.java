760da2a4f636e85621c1e5e61f8922dc0b2676c3
Nerea <bongjourping@gmail.com>
2020-10-20 13:55:30 +0200

    no relevante file

--- leyou-order/src/main/java/com/leyou/repository/IOrderStatusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.leyou.repository;
-
-import com.leyou.order.pojo.OrderStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface IOrderStatusRepository extends JpaRepository<OrderStatus, Long>, JpaSpecificationExecutor<OrderStatus> {
-}
